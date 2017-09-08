b8e2121a6bf5e23ef4cd00aefe5e628bcc4ddd2c
Tommy Carpenter <tommy@research.att.com>
2017-09-08 10:17:44 -0400

    Incorporate broker docker build
    Issue-ID: DCAEGEN2-42
    Change-Id: I7d11400769b7a08bab0ad35da61e3a2dc935f57c
    Signed-off-by: Tommy Carpenter <tommy@research.att.com>

--- mvn-phase-script.sh
+++ mvn-phase-script.sh
@@ -18,44 +18,193 @@
 #
 # ECOMP is a trademark and service mark of AT&T Intellectual Property.
 
+
 echo "running script: [$0] for module [$1] at stage [$2]"
 
-echo "=> Prepare environment "
-#env
+MVN_PROJECT_MODULEID="$1"
+MVN_PHASE="$2"
 
-TIMESTAMP=$(date +%C%y%m%dT%H%M%S) 
-export BUILD_NUMBER="${TIMESTAMP}"
 
-# expected environment variables 
+FQDN="${MVN_PROJECT_GROUPID}.${MVN_PROJECT_ARTIFACTID}"
+if [ "$MVN_PROJECT_MODULEID" == "__" ]; then
+  MVN_PROJECT_MODULEID=""
+fi
+
+if [[ "$MVN_PROJECT_VERSION" == *SNAPSHOT ]]; then
+  echo "=> for SNAPSHOT artifact build"
+  MVN_DEPLOYMENT_TYPE='SNAPSHOT'
+else
+  echo "=> for STAGING/RELEASE artifact build"
+  MVN_DEPLOYMENT_TYPE='STAGING'
+fi
+echo "MVN_DEPLOYMENT_TYPE is             [$DEPLOYMENT_TYPE]"
+
+
+TIMESTAMP=$(date +%C%y%m%dT%H%M%S)
+
+# expected environment variables
 if [ -z "${MVN_NEXUSPROXY}" ]; then
     echo "MVN_NEXUSPROXY environment variable not set.  Cannot proceed"
     exit
 fi
+
 MVN_NEXUSPROXY_HOST=$(echo "$MVN_NEXUSPROXY" |cut -f3 -d'/' | cut -f1 -d':')
-echo "=> Nexu Proxy at $MVN_NEXUSPROXY_HOST, $MVN_NEXUSPROXY"
+echo "=> Nexus Proxy at $MVN_NEXUSPROXY_HOST, $MVN_NEXUSPROXY"
 
-# use the version text detect which phase we are in in LF CICD process: verify, merge, or (daily) release
+if [ -z "$WORKSPACE" ]; then
+    WORKSPACE=$(pwd)
+fi
 
-# mvn phase in life cycle 
+# mvn phase in life cycle
 MVN_PHASE="$2"
+
+
+echo "MVN_PROJECT_MODULEID is            [$MVN_PROJECT_MODULEID]"
+echo "MVN_PHASE is                       [$MVN_PHASE]"
+echo "MVN_PROJECT_GROUPID is             [$MVN_PROJECT_GROUPID]"
+echo "MVN_PROJECT_ARTIFACTID is          [$MVN_PROJECT_ARTIFACTID]"
+echo "MVN_PROJECT_VERSION is             [$MVN_PROJECT_VERSION]"
+echo "MVN_NEXUSPROXY is                  [$MVN_NEXUSPROXY]"
+echo "MVN_RAWREPO_BASEURL_UPLOAD is      [$MVN_RAWREPO_BASEURL_UPLOAD]"
+echo "MVN_RAWREPO_BASEURL_DOWNLOAD is    [$MVN_RAWREPO_BASEURL_DOWNLOAD]"
+MVN_RAWREPO_HOST=$(echo "$MVN_RAWREPO_BASEURL_UPLOAD" | cut -f3 -d'/' |cut -f1 -d':')
+echo "MVN_RAWREPO_HOST is                [$MVN_RAWREPO_HOST]"
+echo "MVN_RAWREPO_SERVERID is            [$MVN_RAWREPO_SERVERID]"
+echo "MVN_DOCKERREGISTRY_DAILY is        [$MVN_DOCKERREGISTRY_DAILY]"
+echo "MVN_DOCKERREGISTRY_RELEASE is      [$MVN_DOCKERREGISTRY_RELEASE]"
+
+expand_templates() 
+{
+  # set up env variables, get ready for template resolution
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_ccsdk_platform_plugins_releases="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.ccsdk.plugins/releases"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_ccsdk_platform_plugins_snapshots="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.ccsdk.plugins/snapshots"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_ccsdk_platform_blueprints_releases="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.ccsdk.blueprints/releases"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_ccsdk_platform_blueprints_snapshots="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.ccsdk.blueprints/snapshots"  export ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_releases="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.dcaegen2/releases"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_snapshots="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.dcaegen2/snapshots"  export ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_plugins_releases="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.dcaegen2.platform.plugins/releases"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_plugins_snapshots="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.dcaegen2.platform.plugins/snapshots"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_blueprints_releases="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.dcaegen2.platform.blueprints/releases"
+  export ONAPTEMPLATE_RAWREPOURL_org_onap_dcaegen2_platform_blueprints_snapshots="$MVN_RAWREPO_BASEURL_DOWNLOAD/org.onap.dcaegen2.platform.blueprints/snapshots"
+
+  export ONAPTEMPLATE_PYPIURL_org_onap_dcaegen2="${MVN_NEXUSPROXY}/content/sites/pypi"
+
+  export ONAPTEMPLATE_DOCKERREGURL_org_onap_dcaegen2_releases="$MVN_DOCKERREGISTRY_DAILY"
+  export ONAPTEMPLATE_DOCKERREGURL_org_onap_dcaegen2_snapshots="$MVN_DOCKERREGISTRY_DAILY/snapshots"
+
+
+  TEMPLATES=$(env |grep ONAPTEMPLATE)
+  echo "====> Resolving the following temaplate from environment variables "
+  echo "[$TEMPLATES]"
+  SELFFILE=$(echo "$0" | rev | cut -f1 -d '/' | rev)
+  for TEMPLATE in $TEMPLATES; do
+    KEY=$(echo "$TEMPLATE" | cut -f1 -d'=')
+    VALUE=$(echo "$TEMPLATE" | cut -f2 -d'=')
+    VALUE2=$(echo "$TEMPLATE" | cut -f2 -d'=' |sed 's/\//\\\//g')
+    FILES=$(grep -rl "$KEY")
+
+    # assuming FILES is not longer than 2M bytes, the limit for variable value max size on this VM
+    for F in $FILES; do
+       if [[ $F == *"$SELFFILE" ]]; then
+          continue
+       fi
+       echo "======> Resolving template $KEY to value $VALUE for file $F"
+       sed -i "s/{{[[:space:]]*$KEY[[:space:]]*}}/$VALUE2/g" "$F"
+
+       #cat "$F"
+    done
+
+    #if [ ! -z "$FILES" ]; then
+    #   echo "====> Resolving template $VALUE to value $VALUE"
+    #   #CMD="grep -rl \"$VALUE\" | tr '\n' '\0' | xargs -0 sed -i \"s/{{[[:space:]]*$VALUE[[:space:]]*}}/$VALUE/g\""
+    #   grep -rl "$KEY" | tr '\n' '\0' | xargs -0 sed -i 's/$KEY/$VALUE2/g'
+    #   #echo $CMD
+    #   #eval $CMD
+    #fi
+  done
+  echo "====> Done template reolving"
+}
+
+build_and_push_docker()
+{
+  IMAGENAME="onap/${FQDN}.${MVN_PROJECT_MODULEID}"
+  IMAGENAME=$(echo "$IMAGENAME" | sed -e 's/_*$//g' -e 's/\.*$//g')
+
+  # use the major and minor version of the MVN artifact version as docker image version
+  VERSION="${MVN_PROJECT_VERSION//[^0-9.]/}"
+  VERSION2=$(echo "$VERSION" | cut -f1-2 -d'.')
+
+  LFQI="${IMAGENAME}:${VERSION}-${TIMESTAMP}"
+  BUILD_PATH="${WORKSPACE}"
+  # build a docker image
+  docker build --rm -f "${WORKSPACE}"/Dockerfile -t "${LFQI}" "${BUILD_PATH}"
+
+  REPO=""
+  if [ $MVN_DEPLOYMENT_TYPE == "SNAPSHOT" ]; then
+     REPO=$MVN_DOCKERREGISTRY_DAILY
+  elif [ $MVN_DEPLOYMENT_TYPE == "STAGING" ]; then
+     # there seems to be no staging docker registry?  set to use SNAPSHOT also
+     #REPO=$MVN_DOCKERREGISTRY_RELEASE
+     REPO=$MVN_DOCKERREGISTRY_DAILY
+  else
+     echo "Fail to determine DEPLOYMENT_TYPE"
+     REPO=$MVN_DOCKERREGISTRY_DAILY
+  fi
+  echo "DEPLOYMENT_TYPE is: $MVN_DEPLOYMENT_TYPE, repo is $REPO"
+
+  if [ ! -z "$REPO" ]; then
+    USER=$(xpath -e "//servers/server[id='$REPO']/username/text()" "$SETTINGS_FILE")
+    PASS=$(xpath -e "//servers/server[id='$REPO']/password/text()" "$SETTINGS_FILE")
+    if [ -z "$USER" ]; then
+      echo "Error: no user provided"
+    fi
+    if [ -z "$PASS" ]; then
+      echo "Error: no password provided"
+    fi
+    [ -z "$PASS" ] && PASS_PROVIDED="<empty>" || PASS_PROVIDED="<password>"
+    echo docker login "$REPO" -u "$USER" -p "$PASS_PROVIDED"
+    docker login "$REPO" -u "$USER" -p "$PASS"
+
+    if [ $MVN_DEPLOYMENT_TYPE == "SNAPSHOT" ]; then
+      REPO="$REPO/snapshots"
+    elif [ $MVN_DEPLOYMENT_TYPE == "STAGING" ]; then
+      # there seems to be no staging docker registry?  set to use SNAPSHOT also
+      #REPO=$MVN_DOCKERREGISTRY_RELEASE
+      REPO="$REPO"
+    else
+      echo "Fail to determine DEPLOYMENT_TYPE"
+      REPO="$REPO/unknown"
+    fi
+
+    OLDTAG="${LFQI}"
+    PUSHTAGS="${REPO}/${IMAGENAME}:${VERSION2}-${TIMESTAMP} ${REPO}/${IMAGENAME}:${VERSION2} ${REPO}/${IMAGENAME}:${VERSION2}-latest"
+    for NEWTAG in ${PUSHTAGS}
+    do
+      echo "tagging ${OLDTAG} to ${NEWTAG}"
+      docker tag "${OLDTAG}" "${NEWTAG}"
+      echo "pushing ${NEWTAG}"
+      docker push "${NEWTAG}"
+      OLDTAG="${NEWTAG}"
+    done
+  fi
+
+}
 
 case $MVN_PHASE in
 clean)
   echo "==> clean phase script"
+  rm -rf _build/;
   ;;
 generate-sources)
   echo "==> generate-sources phase script"
+  echo "TODO: Might need expand templates back here later"
+  #expand_templates
   ;;
 compile)
   echo "==> compile phase script"
-  echo "=====> building "
-  rm -rf _build/;
   /usr/bin/rebar3 upgrade;
   /usr/bin/rebar3 release;
   ;;
 test)
   echo "==> test phase script"
-  echo "=====> testing "
   /usr/bin/rebar3 eunit
   ;;
 package)
@@ -66,8 +215,11 @@ install)
   ;;
 deploy)
   echo "==> deploy phase script"
+  #upload_wagons_and_type_yamls
+  build_and_push_docker
   ;;
 *)
   echo "==> unprocessed phase"
   ;;
 esac
+
